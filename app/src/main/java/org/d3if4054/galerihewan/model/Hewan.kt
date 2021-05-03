22638fedfa153609198a5a2a5292a574f9ebf28e
kelvin <kelvinpradiza@gmail.com>
2021-05-03 23:01:02 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4054/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4054/galerihewan/model/Hewan.kt
@@ -1,7 +1,9 @@
 package org.d3if4054.galerihewan.model
 
+import org.d3if4054.galerihewan.R
+
 data class Hewan (
     val nama: String,
     val namaLatin: String,
-    val imagesResId: Int
+    val imagesResId: Int = R.drawable.angsa
 )
