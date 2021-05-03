3403e15875b628386e08ac06c103cf32f4374179
kelvin <kelvinpradiza@gmail.com>
2021-05-03 22:42:43 +0700

    Membuat view model untuk fitur main

--- app/src/main/java/org/d3if4054/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4054/galerihewan/MainActivity.kt
@@ -2,18 +2,11 @@ package org.d3if4054.galerihewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
-import org.d3if4054.galerihewan.databinding.ActivityMainBinding
-import org.d3if4054.galerihewan.ui.main.MainAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         }
     }
